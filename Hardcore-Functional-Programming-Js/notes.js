d94f13d15732eb6dbb42badf5b561f2772ab93df
“Johan <johan.suarez@unosquare.com>
2022-05-24 16:03:43 -0500

    course done

--- Hardcore-Functional-Programming-Js/notes.js
+++ Hardcore-Functional-Programming-Js/notes.js
@@ -44,10 +44,39 @@ const addStepOne = curry(add)(1); //first one parameter
 const addFinalStep = addStepOne(2); //then second parameter
 console.log(addFinalStep);
 
+//Loggin
+const log = curry((tag, value) => (console.log(tag, value), value));
+
 //Compose functions
 
 const uppercase = str => str.toUpperCase();
 const addQuestion = str => str + "?";
 const compose = (f, g) => x => f(g(x));
-const resultCompose = compose(addQuestion, uppercase);
+const resultCompose = compose(
+  addQuestion,
+  compose(log("upercasse:"), uppercase)
+);
 console.log(resultCompose("hello"));
+
+//Ramda is a library that helps with all this functions, curry, composition, etc:  https://ramdajs.com.
+
+//Functors: "is any object we can map and apply a function generating another object instance of the same type and connections."
+
+const Box = x => ({
+  map: f => Box(f(x)),
+  fold: f => f(x),
+});
+
+function copToMex(pesos) {
+  const double = pesos * 2;
+  const twoCeros = double * 100;
+  return twoCeros;
+}
+
+console.log(copToMex(8500000));
+
+const copToMexFP = Box(8500000)
+  .map(x => x * 2)
+  .fold(x => x * 100);
+
+console.log(copToMexFP);
